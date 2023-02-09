f42ea3185ec436969a5d09c3b241f6c25a795afa
Javion Wiley <javionw@Admin-5>
2023-02-09 10:26:17 -0600

    Added Js to webpage

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function whodat() {
+   const username = prompt("what is your name?");
+   if(username != null) {
+       alert("hello, " + username);
+   }else{
+        alert("hello");
+   }
+}
+whodat();
+function newtocoding() {
+    const answer = prompt("Are you new to coding?");
+    if(answer == "yes") {
+        alert("Start on the Markdown section")
+    }else if(answer == "no") {
+        alert("Share with someone who is!")
+
+    }else{
+        alert("Welcome!")
+    }   
+}
+newtocoding();
+function construction() {
+    alert("Reminder! This page is always under construction!")
+}
+construction();
+// applepie();
+
+// pie2(9);
+
+
+// console.log(ingredientX);
