ae6c5780250c897c1f15a99e85645e3e7d3b2f3d
Javion Wiley <javionw@Admin-5>
2023-02-13 15:03:23 -0600

    updated pages

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 function whodat() {
    const username = prompt("what is your name?");
    if(username != null) {
-       alert("hello, " + username);
+       alert("What's Up, " + username);
    }else{
-        alert("hello");
+        alert("What's Up");
    }
 }
 whodat();
 function newtocoding() {
     const answer = prompt("Are you new to coding?");
     if(answer == "yes") {
-        alert("Start on the Markdown section")
+        alert("Start in the Markdown section")
     }else if(answer == "no") {
-        alert("Share with someone who is!")
+        alert("Share with someone who is! I hope you enjoy the Page!")
 
     }else{
         alert("Welcome!")
@@ -24,6 +24,27 @@ function construction() {
     alert("Reminder! This page is always under construction!")
 }
 construction();
+
+var feild = document.quertSelector('textarea');
+var backUp = feild.getAttribute('placeholder');
+var btn = document.querySelector('.btn');
+var clear = document.getElementById('clear') 
+
+feild.onfocus = function(){
+    this.setAttribute('placeholder','');
+    this.style.borderColor = '#333 ';
+    btn.style.display = 'block' 
+}
+feild.onblur = function(){
+    this.setAttribute('placeholder',backUp)
+    this.style.bordercolor = '#aaa'
+}
+clear.onclick = function(){
+
+    btn.style.display = "none";
+    feild.value = '';
+}
+
 // applepie();
 
 // pie2(9);
